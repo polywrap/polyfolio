439d03898195bca942d99a3c67f84c26b5fc5823
Вова Барибін <oknalogia@outlook.com>
2022-03-04 23:34:37 +0200

    was fixed wrong import

--- src/pages/SettingsPage/SettingsPage.tsx
+++ src/pages/SettingsPage/SettingsPage.tsx
@@ -1,47 +1,31 @@
 import React from 'react';
 
-import useTheme from 'common/hooks/useTheme/useTheme';
-import classNames from 'classnames';
-import style from './SettingsPage.module.scss';
+import styles from './SettingsPage.module.scss';
 
-import useTranslation from 'common/hooks/useTranslation/useTranslation';
-import ProfileWallet from 'common/components/ProfileWallet/ProfileWallet';
-import useAuth from 'common/hooks/useAuth/useAuth';
-import Icon from 'common/components/Icon/Icon';
-import iconsObj from 'assets/icons/iconsObj';
-import ThemeSwitcher from 'common/components/ThemeSwitcher/ThemeSwitcher';
-import CurrencyPicker from 'common/components/CurrencyPicker/CurrencyPicker';
+import classNames from 'classnames';
+
+import Header from 'common/components/Header/Header';
+import Footer from 'common/components/Footer/Footer';
+import useTheme from 'common/hooks/useTheme/useTheme';
+import Sidebar from 'common/components/Sidebar/Sidebar';
+import SettingsBody from './SettingsBody/SettingsBody';
 
 function SettingsPage() {
   const theme = useTheme()
-  const { user } = useAuth()
-  const translation = useTranslation()
 
   return (
-    <div className={classNames(style[theme])}>
-      <div className={style.wrapper}>
-        <div className={style.head_title}>Settings</div>
-        <div>
-          <div className={style.profile}>Profile</div>
-          <div>
-            <Icon src={iconsObj.profile} />
-            <ProfileWallet address={user} size={3} />
+    <div className={classNames(styles[theme])}>
+      <Header />
+      <div className={styles.container}>
+        <aside className={styles.aside}>
+          <Sidebar />
+        </aside>
+        <main className={styles.main}>
+          <div className={styles.wrapper}>
+            <SettingsBody />
           </div>
-        </div>
-        <div className={style.settings_prop}>
-          <div className={style.row}>
-            <div className={style.left}>Theme</div>
-            <div className={style.right}><ThemeSwitcher /></div>
-          </div>
-          <div className={style.row}>
-            <div className={style.left}>Base Currency</div>
-            <div className={style.right}><CurrencyPicker /></div>
-          </div>
-          <div className={style.row}>
-            <div className={style.left}>Language</div>
-            <div className={style.right}></div>
-          </div>
-        </div>
+          <Footer wrapperClassName={styles.footer} />
+        </main>
       </div>
     </div>
   )
