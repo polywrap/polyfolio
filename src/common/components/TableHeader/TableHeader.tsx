b2ffec412007086e006ecde682d4888c3ddad413
yuliia-gorgozka <taragorgodze@gmail.com>
2022-03-07 15:45:01 +0200

    fix styles

--- src/common/components/TableHeader/TableHeader.tsx
+++ src/common/components/TableHeader/TableHeader.tsx
@@ -1,21 +1,20 @@
-import React, { useState, useCallback } from 'react';
+import React, {useState, useCallback} from 'react';
 
 import classNames from 'classnames';
 import useTheme from 'common/hooks/useTheme/useTheme';
 import style from './TableHeader.module.scss';
 import iconsObj from 'assets/icons/iconsObj';
-import Icon from 'common/components/Icon/Icon';
 import Input from 'common/components/Input/Input';
 import Dropdown from '../Dropdown/Dropdown';
 import TablePagination from '../TablePagination/TablePagination';
-import { filteredDropdown } from 'utils/filteredDropdown';
-import { dropdownItems } from './TableHeader.config';
+import {filteredDropdown} from 'utils/helpers';
+import {dropdownItems} from './TableHeader.config';
 
 function TableHeader() {
-  const total = 2
-  const theme = useTheme()
-  const [value, setValue] = useState<string>()
-  const [page, setPage] = useState<number>(1)
+  const total = 2;
+  const theme = useTheme();
+  const [value, setValue] = useState<string>();
+  const [page, setPage] = useState<number>(1);
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const [currency, setCurrency] = useState(dropdownItems[0]);
 
@@ -24,9 +23,12 @@ function TableHeader() {
     setIsOpen(false);
   };
 
-  const onPageChange = useCallback((nextPage: number) => {
-    setPage(nextPage)
-  }, [setPage])
+  const onPageChange = useCallback(
+    (nextPage: number) => {
+      setPage(nextPage);
+    },
+    [setPage],
+  );
 
   return (
     <div className={classNames(style[theme], style.controlls)}>
@@ -36,7 +38,7 @@ function TableHeader() {
           onChange={setValue}
           icon={iconsObj.search}
           wrapperClassName={classNames(style.input)}
-          placeholder='Search by date or event type (ex: send)'
+          placeholder="Search by date or event type (ex: send)"
         />
       </div>
       <div className={style.right}>
@@ -55,7 +57,7 @@ function TableHeader() {
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default TableHeader
+export default TableHeader;
