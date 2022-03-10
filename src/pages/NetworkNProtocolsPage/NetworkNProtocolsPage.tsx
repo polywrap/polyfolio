8b6d87fd22e63f97ba4a2dc6b01f2797fb0ffd31
Вова Барибін <oknalogia@outlook.com>
2022-03-10 22:39:56 +0200

    was created network and protocol page

---
+++ src/pages/NetworkNProtocolsPage/NetworkNProtocolsPage.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+
+import classNames from 'classnames';
+import iconsObj from 'assets/icons/iconsObj';
+import styles from './NetworkNProtocolsPage.module.scss';
+
+import { useLocation } from 'react-router-dom';
+import Header from 'common/components/Header/Header';
+import Footer from 'common/components/Footer/Footer';
+import useTheme from 'common/hooks/useTheme/useTheme';
+import Sidebar from 'common/components/Sidebar/Sidebar';
+import HeaderPage from 'common/components/HeaderPage/HeaderPage';
+import VaultsTable from 'common/components/VaultsTable/VaultsTable';
+import AssetsTable from 'common/components/AssetsTable/AssetsTable';
+import useTranslation from 'common/hooks/useTranslation/useTranslation';
+import ProtocolsTable from 'common/components/ProtocolsTable/ProtocolsTable';
+import HeaderCurrencyPage from 'pages/HeaderCurrencyPage/HeaderCurrencyPage';
+import AssetTransaction from 'common/components/AssetTransaction/AssetTransaction';
+
+function NetworkNProtocolsPage() {
+  const theme = useTheme();
+  const { pathname } = useLocation();
+  const translation = useTranslation();
+
+  const path = pathname.split('/')[1];
+
+  return (
+    <div className={classNames(styles.landing_page, styles[theme])}>
+      <Header inputClassName={styles.input} />
+      <div className={styles.container}>
+        <aside className={styles.aside}>
+          <Sidebar />
+        </aside>
+        <main className={styles.main}>
+          <div className={styles.wrapper}>
+            <HeaderPage
+              icon={iconsObj.assetsToken}
+              title={translation.Assets.grtSecondary}
+              secondaryTitle={translation.Assets.grt}  
+            />
+            <HeaderCurrencyPage />
+            {
+              path === 'network' ? (
+                <>
+                  <AssetsTable />
+                  <ProtocolsTable />
+                </>
+              ) : (
+                <>
+                  <VaultsTable />
+                  <AssetTransaction />
+                </>
+              )
+            }
+          </div>
+          <Footer wrapperClassName={styles.footer} />
+        </main>
+      </div>
+    </div>
+  );
+}
+
+export default NetworkNProtocolsPage;
