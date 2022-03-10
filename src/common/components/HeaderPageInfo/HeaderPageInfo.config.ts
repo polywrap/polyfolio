62416feb89a37ea53f7951de16ee11f4ca524df4
Вова Барибін <oknalogia@outlook.com>
2022-03-10 23:10:34 +0200

    was ejected some html from existed component

---
+++ src/common/components/HeaderPageInfo/HeaderPageInfo.config.ts
@@ -0,0 +1,10 @@
+import {HeaderInfoTypes} from './HeaderPageInfo.types';
+
+const content: HeaderInfoTypes = {
+  title: 13337337,
+  value: 0.56,
+  percent: 0.02,
+  id: 1,
+};
+
+export {content};
---
+++ src/common/components/HeaderPageInfo/HeaderPageInfo.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import classNames from 'classnames';
+
+import styles from './HeaderPageInfo.module.scss';
+
+import useTheme from 'common/hooks/useTheme/useTheme';
+import { content } from './HeaderPageInfo.config';
+import numberFormatter from 'utils/numberFormatter';
+import useTranslation from 'common/hooks/useTranslation/useTranslation';
+
+function HeaderPageInfo() {
+  const theme = useTheme();
+  const translation = useTranslation();
+
+  return (
+    <div className={classNames(styles[theme], styles.headerPageInfoContainer)}>
+      <span className={styles.secondaryTitle}>{translation.Dashboard.secondaryTitle}</span>
+      <h2>${numberFormatter({ value: content.title, size: 2 })}</h2>
+      <div className={styles.secondaryValue}>
+        <div className={styles.value}>
+          +{numberFormatter({ value: content.percent, size: 2 })}%
+        </div>
+        <div className={classNames(styles.value, styles.percent)}>
+          +${numberFormatter({ value: content.value, size: 2 })}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default HeaderPageInfo;
