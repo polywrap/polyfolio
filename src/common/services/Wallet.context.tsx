c13088c481f1e93e9f74fa980fcdb85051981469
Yuliia <67199866+yuliiagorgozka@users.noreply.github.com>
2022-03-16 12:53:52 +0200

    fix bulid (#69)

--- src/common/services/Wallet.context.tsx
+++ src/common/services/Wallet.context.tsx
@@ -1,43 +1,41 @@
-import React, { createContext, useContext, useState, useEffect, useRef } from 'react';
+import React, {createContext, useContext, useState} from 'react';
 //import MetaMaskOnboarding from '@metamask/onboarding';
 
-import { WalletContextProps } from './Wallet.types';
+import {WalletContextProps} from './Wallet.types';
 
-import { ethers } from 'ethers';
+import {ethers} from 'ethers';
 import useAuth from 'common/hooks/useAuth/useAuth';
 
 const WalletContext = createContext<WalletContextProps>(null);
 export const useWalletContext = () => useContext(WalletContext);
 
-function WalletContextProvider({ children }: { children: React.ReactNode }) {
-  const { logIn, setUser } = useAuth();
+function WalletContextProvider({children}: {children: React.ReactNode}) {
+  const {logIn, setUser} = useAuth();
   const [wallet, setWallet] = useState<Record<string, unknown>>();
 
   const connect = async () => {
-    const provider = new ethers.providers.Web3Provider(window["ethereum"]);
+    const provider = new ethers.providers.Web3Provider(window['ethereum']);
     await provider.send('eth_requestAccounts', []);
     const signer = provider.getSigner();
-    setWallet({ provider, signer });
+    setWallet({provider, signer});
     logIn(await signer.getAddress());
-  }
+  };
 
   const check = async (address: string[]) => {
     if (address.length > 0) {
-      const provider = new ethers.providers.Web3Provider(window["ethereum"]);
+      const provider = new ethers.providers.Web3Provider(window['ethereum']);
       await provider.send('eth_requestAccounts', []);
       const signer = provider.getSigner();
-      setWallet({ provider, signer });
+      setWallet({provider, signer});
       setUser(await signer.getAddress());
     } else {
       setWallet(null);
       setUser(null);
     }
-  }
+  };
 
   return (
-    <WalletContext.Provider value={{ wallet, check, connect }}>
-      {children}
-    </WalletContext.Provider>
+    <WalletContext.Provider value={{wallet, check, connect}}>{children}</WalletContext.Provider>
   );
 }
 
