f0dd5927ac2b995213550434cd111dbcbf859172
Вова Барибін <oknalogia@outlook.com>
2022-03-18 12:08:16 +0200

    was called api

--- src/pages/DashboardPage/DashboardPage.tsx
+++ src/pages/DashboardPage/DashboardPage.tsx
@@ -1,43 +1,45 @@
-import React, { ReactNode/* , useEffect */ } from 'react';
+import React, { ReactNode, useEffect } from 'react';
 
 import styles from './DashboardPage.module.scss';
 
 import classNames from 'classnames';
 
+import { useWeb3ApiQuery } from '@web3api/react';
+import useAuth from 'common/hooks/useAuth/useAuth';
 import Header from 'common/components/Header/Header';
 import Footer from 'common/components/Footer/Footer';
 import useTheme from 'common/hooks/useTheme/useTheme';
 import Sidebar from 'common/components/Sidebar/Sidebar';
-//import { useWeb3ApiClient, useWeb3ApiQuery } from '@web3api/react';
 
 function DashboardPage({ children }: { children: ReactNode }) {
+  const {user} = useAuth()
   const theme = useTheme();
-  //const client = useWeb3ApiClient();
-  //const query = useWeb3ApiQuery();
 
-  // example
-  /* useEffect(() => {
-    client.query({
-      uri: 'ipfs/QmRYP5qwQd7AotVbtcx7KhN8HuHX9DCg8sS9LVE4kstpVw',
-      query: '',
-    })
-  }, []) */
+  useEffect(() => {
+    const getData = async () => {
+      const data = await execute({ 
+        accountAddress: user,
+        vsCurrencies: [],
+        noTruncate: false,
+        underlyingPrice: false,
+      })
+      console.log(data)
+    }
 
-  //examle from another project
-  /* const { execute } = useWeb3ApiQuery({
+    getData()
+  }, [])
+
+  const { execute } = useWeb3ApiQuery({
     uri: `ipfs/QmRYP5qwQd7AotVbtcx7KhN8HuHX9DCg8sS9LVE4kstpVw`,
-    query: `mutation {
-    registerDomain(
-      domain: $domain
-      owner: $owner
-      registrarAddress: $registrarAddress
-      registryAddress: $registryAddress
-      network: $network
-    )
-  }`,
+    query: `query {
+      getAccountBalance(
+        accountAddress: $accountAddress
+        vsCurrencies: $vsCurrencies
+        noTruncate: $noTruncate
+        underlyingPrice: $underlyingPrice
+      )
+    }`,
   });
-
-  await execute({ domain: 'dfasfd.sad', owner: 'dsfasdf', ...}) */
 
   return (
     <div className={classNames(styles.landing_page, styles[theme])}>
