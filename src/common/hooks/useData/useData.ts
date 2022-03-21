ca33035ee8afcef6a4cdf7fed72de785afbeba0f
Вова Барибін <oknalogia@outlook.com>
2022-03-21 20:41:39 +0200

    was added hook for data receiving

---
+++ src/common/hooks/useData/useData.ts
@@ -0,0 +1,46 @@
+import {useEffect} from 'react';
+import {atom, useRecoilState} from 'recoil';
+
+import { useWeb3ApiQuery } from '@web3api/react';
+import useAuth from '../useAuth/useAuth';
+
+const BALANCE_STATE_KEY = 'polyfolio_balance';
+
+export default function useData() {
+  const {user} = useAuth();
+  const balancePersistState = atom({
+    key: BALANCE_STATE_KEY,
+    default: null,
+  });
+  
+  const [balance, setBalance] = useRecoilState(balancePersistState);
+
+  const { execute } = useWeb3ApiQuery({
+    uri: `ipfs/QmRYP5qwQd7AotVbtcx7KhN8HuHX9DCg8sS9LVE4kstpVw`,
+    query: `query {
+      getAccountBalance(
+        accountAddress: $accountAddress
+        vsCurrencies: $vsCurrencies
+        noTruncate: $noTruncate
+        underlyingPrice: $underlyingPrice
+      )
+    }`,
+  });
+
+  useEffect(() => {
+    const getData = async () => {
+      const data = await execute({ 
+        accountAddress: user,
+        vsCurrencies: [],
+        noTruncate: false,
+        underlyingPrice: false,
+      })
+      setBalance(data?.data?.getAccountBalance)
+    }
+
+    getData()
+  }, [user])
+
+
+  return {balance};
+}
--- src/pages/DashboardPage/DashboardPage.tsx
+++ src/pages/DashboardPage/DashboardPage.tsx
@@ -4,42 +4,17 @@ import styles from './DashboardPage.module.scss';
 
 import classNames from 'classnames';
 
-import { useWeb3ApiQuery } from '@web3api/react';
-import useAuth from 'common/hooks/useAuth/useAuth';
 import Header from 'common/components/Header/Header';
 import Footer from 'common/components/Footer/Footer';
 import useTheme from 'common/hooks/useTheme/useTheme';
 import Sidebar from 'common/components/Sidebar/Sidebar';
+import useData from 'common/hooks/useData/useData';
 
 function DashboardPage({ children }: { children: ReactNode }) {
-  const {user} = useAuth()
+  const {balance} = useData();
   const theme = useTheme();
 
-  useEffect(() => {
-    const getData = async () => {
-      const data = await execute({ 
-        accountAddress: user,
-        vsCurrencies: [],
-        noTruncate: false,
-        underlyingPrice: false,
-      })
-      console.log(data)
-    }
-
-    getData()
-  }, [])
-
-  const { execute } = useWeb3ApiQuery({
-    uri: `ipfs/QmRYP5qwQd7AotVbtcx7KhN8HuHX9DCg8sS9LVE4kstpVw`,
-    query: `query {
-      getAccountBalance(
-        accountAddress: $accountAddress
-        vsCurrencies: $vsCurrencies
-        noTruncate: $noTruncate
-        underlyingPrice: $underlyingPrice
-      )
-    }`,
-  });
+  console.log(balance)
 
   return (
     <div className={classNames(styles.landing_page, styles[theme])}>
