fe0701c36af9d4f468f82f6e347c46f1d3f35d06
Вова Барибін <oknalogia@outlook.com>
2022-03-23 15:58:48 +0200

    was formated received data

--- src/common/hooks/useData/useData.ts
+++ src/common/hooks/useData/useData.ts
@@ -1,10 +1,13 @@
 import {useEffect} from 'react';
 import {atom, useRecoilState} from 'recoil';
+import { v4 as uuidv4 } from 'uuid';
 
 import { useWeb3ApiQuery } from '@web3api/react';
 import useAuth from '../useAuth/useAuth';
 
 const BALANCE_STATE_KEY = 'polyfolio_balance';
+const ALL_ASSETS_STATE_KEY = 'polyfolio_allAssets';
+const ALL_ASSETS_SUM_STATE_KEY = 'polyfolio_allAssetsSum';
 
 export default function useData() {
   const {user} = useAuth();
@@ -12,8 +15,18 @@ export default function useData() {
     key: BALANCE_STATE_KEY,
     default: null,
   });
+  const allAssetsState = atom({
+    key: ALL_ASSETS_STATE_KEY,
+    default: null,
+  });
+  const allAssetsSumState = atom({
+    key: ALL_ASSETS_SUM_STATE_KEY,
+    default: null,
+  });
   
   const [balance, setBalance] = useRecoilState(balanceState);
+  const [allAssets, setAllAssets] = useRecoilState(allAssetsState);
+  const [allAssetsSum, setAllAssetsSum] = useRecoilState(allAssetsSumState);
 
   const { execute } = useWeb3ApiQuery({
     uri: `ipfs/QmRYP5qwQd7AotVbtcx7KhN8HuHX9DCg8sS9LVE4kstpVw`,
@@ -27,6 +40,16 @@ export default function useData() {
     }`,
   });
 
+  const formatData = data => {
+    const formatedData = data.protocols.map(item => {
+      const asset = formatAsset(item.assets);
+
+      return {...item, assets: asset};
+    })
+
+    return {...data, protocols: formatedData};
+  }
+
   useEffect(() => {
     const getData = async () => {
       const data = await execute({ 
@@ -35,12 +58,49 @@ export default function useData() {
         noTruncate: false,
         underlyingPrice: false,
       })
-      setBalance(data?.data?.getAccountBalance)
+      setBalance(formatData(data?.data?.getAccountBalance))
     }
 
     getData()
   }, [user])
 
+  const ejectAssetData = () => {
+    const assetArray = [];
 
-  return {balance};
+    if (balance) {
+      balance.protocols.map(item => {
+        for (const key in item.assets) {
+          assetArray.push(item.assets[key]);
+        }
+      });
+    }
+
+    return assetArray;
+  }
+
+  const formatAsset = assets => 
+    assets.map(item => { return {...item.balance.token, id: uuidv4()} })
+
+  useEffect(() => {
+    if (balance) {
+      setAllAssets(ejectAssetData());
+    }
+  }, [balance])
+
+  const getAllAssetSum = () => {
+    let sum = 0;
+
+    if (allAssets) {
+      allAssets.map(item => item.values.map(val => sum += Number(val.value.split(',').join(''))));
+  
+      setAllAssetsSum(sum);
+    }
+  }
+
+  useEffect(() => {
+    getAllAssetSum();
+  }, [allAssets])
+
+
+  return {balance, allAssets, allAssetsSum};
 }
--- src/pages/DashboardPage/Portfolio/Portfolio.tsx
+++ src/pages/DashboardPage/Portfolio/Portfolio.tsx
@@ -1,45 +1,13 @@
-import React, {useEffect} from 'react';
-import {atom, useRecoilState} from 'recoil';
+import React from 'react';
 import Networks from 'common/components/Networks/Networks';
 import ProtocolsTable from 'common/components/ProtocolsTable/ProtocolsTable';
 import AssetsTable from 'common/components/AssetsTable/AssetsTable';
 import DashboardNavigation from 'common/components/DashboardNavigation/DashboardNavigation';
 import HeaderDashboard from 'common/components/HeaderDashboard/HeaderDashboard';
 import DashboardPage from '../DashboardPage';
-import useData from 'common/hooks/useData/useData';
 
-const ASSETS_STATE_KEY = 'polyfolio_all_assets';
 
 function Portfolio() {
-  const {balance} = useData();
-
-  const assetsState = atom({
-    key: ASSETS_STATE_KEY,
-    default: null,
-  });
-
-  const [assets, setAssets] = useRecoilState(assetsState);
-
-  const ejectAssetData = () => {
-    const assetArray = [];
-
-    if (balance) {
-      balance.protocols.map(item => {
-        for (const key in item.assets) {
-          assetArray.push(item.assets[key]);
-        }
-      });
-    }
-
-    return assetArray;
-  }
-
-  useEffect(() => {
-    if (balance) {
-      setAssets(ejectAssetData());
-    }
-  }, [balance])
-
   return (
     <DashboardPage>
       <HeaderDashboard />
