593f3f13ef2dfcf3cf21a1087e65bf91640497ec
Вова Барибін <oknalogia@outlook.com>
2022-04-01 15:29:30 +0300

    was written logic for wallet recording to LC

--- src/common/services/Wallet.context.tsx
+++ src/common/services/Wallet.context.tsx
@@ -1,35 +1,45 @@
-import React, {createContext, useContext, useState} from 'react';
-//import MetaMaskOnboarding from '@metamask/onboarding';
-
-import {WalletContextProps} from './Wallet.types';
-
+import React, {createContext, useContext} from 'react';
 import {ethers} from 'ethers';
+import {WalletContextProps, IWallet} from './Wallet.types';
 import useAuth from 'common/hooks/useAuth/useAuth';
+import {atom, useRecoilState} from 'recoil';
 
+import useLocalStorage from 'common/hooks/useLocalStorage/useLocalStorage';
+
+const WALLET_LS_STATE = 'polyfolio_wallet';
+const WALLET_STATE_KEY = 'polyfolio_wallet_state';
 const WalletContext = createContext<WalletContextProps>(null);
 export const useWalletContext = () => useContext(WalletContext);
 
 function WalletContextProvider({children}: {children: React.ReactNode}) {
   const {logIn, setUser} = useAuth();
-  const [wallet, setWallet] = useState<Record<string, unknown>>();
+  const [persistedWallet, setPersistedWallet] = useLocalStorage<IWallet>(WALLET_LS_STATE, null);
+  const walletPersistState = atom<IWallet>({
+    key: WALLET_STATE_KEY,
+    default: persistedWallet,
+  });
+  const [wallet, setWallet] = useRecoilState(walletPersistState);
 
   const connect = async () => {
     const provider = new ethers.providers.Web3Provider(window['ethereum']);
     await provider.send('eth_requestAccounts', []);
     const signer = provider.getSigner();
     setWallet({provider, signer});
+    setPersistedWallet({provider, signer});
     logIn(await signer.getAddress());
   };
 
   const check = async (address: string[]) => {
     if (address.length > 0) {
-      const provider = new ethers.providers.Web3Provider(window['ethereum']);
-      await provider.send('eth_requestAccounts', []);
-      const signer = provider.getSigner();
-      setWallet({provider, signer});
-      setUser(await signer.getAddress());
+      console.log(wallet)
+      await wallet?.provider.send('eth_requestAccounts', []);
+      const signer = await wallet?.provider.getSigner();
+      setWallet({...wallet, signer});
+      setPersistedWallet({...wallet, signer});
+      setUser(await signer?.getAddress());
     } else {
       setWallet(null);
+      setPersistedWallet(null);
       setUser(null);
     }
   };
--- src/common/services/Wallet.types.ts
+++ src/common/services/Wallet.types.ts
@@ -1,5 +1,10 @@
+export interface IWallet {
+  provider: import ('ethers').ethers.providers.Web3Provider;
+  signer: import ('ethers').ethers.providers.JsonRpcSigner;
+}
+
 export interface WalletContextProps {
-  wallet: Record<string, unknown>;
+  wallet: IWallet;
   check: (address: string[]) => void;
   connect: () => void;
 }
