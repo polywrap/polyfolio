49123364cc38b2346594dc92441fc9a9024ecb24
Вова Барибін <oknalogia@outlook.com>
2022-05-02 22:14:59 +0300

    added forgotten types

--- src/common/networks/Networks.context.tsx
+++ src/common/networks/Networks.context.tsx
@@ -2,7 +2,7 @@ import React, {useEffect, useContext, createContext} from 'react';
 import {atom, useRecoilState} from 'recoil';
 
 import {networks} from './Networks.config';
-import {NetworksContextProps} from './Networks.types';
+import {NetworksContextProps, INetworks} from './Networks.types';
 
 const NETWORKS_STATE_KEY = 'polyfolio_network_state';
 
@@ -10,7 +10,7 @@ const NetworksContext = createContext<NetworksContextProps>(null);
 export const useNetworks = () => useContext(NetworksContext);
 
 export default function NetworksContextProvider({children}) {
-  const networkPersistState = atom({
+  const networkPersistState = atom<INetworks[]>({
     key: NETWORKS_STATE_KEY,
     default: null,
   });
