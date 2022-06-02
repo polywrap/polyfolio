873cd7786e1ae2e7313d0032de8451742eb45101
Mike <mikelrap@gmail.com>
2022-06-02 16:07:46 +0300

    TokenView finished

--- src/common/components/AssetTransaction/AssetTransactionItem/TokenView.tsx
+++ src/common/components/AssetTransaction/AssetTransactionItem/TokenView.tsx
@@ -9,6 +9,7 @@ import {getTokenAmount} from 'utils/dataFormatting';
 import useToken from 'common/hooks/useToken/useToken';
 import {useCurrency} from 'common/currency/Currency.context';
 import {CurrencySymbol} from 'common/currency/Currency.types';
+import useTokenView from '../useTokenView';
 
 export interface TokenViewProps {
   id: string;
@@ -17,61 +18,26 @@ export interface TokenViewProps {
 }
 
 const TokenView = ({token}: {token: TokenViewProps}) => {
-  const tokenMetadata = useAssetMetadata(token.id, 1, token.tokenAddress);
-  const tokenInfo = useToken(token?.tokenAddress);
-  const {currency} = useCurrency();
-  const tokenAmount: string = tokenInfo
-    ? getTokenAmount(token?.tokenValue, tokenInfo.decimals)
-    : '';
-
-  const tokenPrice = tokenMetadata?.market_data.current_price.find(
-    (p) => p.currency === currency.toLocaleLowerCase(),
-  );
-
-  const getTokenAmountInCurrency = () => {
-    if (token.tokenValue && tokenPrice) {
-      return Number(tokenAmount) * Number(tokenPrice.price);
-    }
-  };
-  const tokenAmountInCurrency = getTokenAmountInCurrency();
-
-  console.log(currency);
-
-  const currStr = CurrencySymbol[currency] || '';
+  const {tokenInfo, loading, error} = useTokenView(token);
 
   return (
     <div key={token.id} className={classNames(style.flex_unit, style.token)}>
-      {tokenMetadata ? (
+      {tokenInfo ? (
         <>
           <div className={style.img_container}>
-            <Icon
-              src={tokenMetadata?.image.small}
-              className={classNames(style.icon)}
-              alt={tokenMetadata?.id}
-            />
+            <Icon src={tokenInfo.icon} className={classNames(style.icon)} alt={tokenInfo?.symbol} />
           </div>
           <div className={style.text}>
             <div className={style.strong}>
-              {token?.tokenValue ? (
-                <>
-                  {tokenAmount.startsWith('-') ? '' : '+'}
-                  {numberFormatter({value: tokenAmount, size: 2})} {tokenInfo?.symbol}
-                </>
-              ) : (
-                tokenInfo?.symbol
-              )}
+              {tokenInfo?.tokenChange ? tokenInfo.tokenChange : tokenInfo.symbol}
             </div>
-            <div className={style.common}>
-              {tokenAmountInCurrency
-                ? (tokenAmountInCurrency.toString().startsWith('-') ? '-' : '+') +
-                  currStr +
-                  numberFormatter({value: tokenAmountInCurrency, size: 1}).replace('-', '')
-                : ''}
-            </div>
+            <div className={style.common}>{tokenInfo?.currencyChange}</div>
           </div>
         </>
-      ) : (
+      ) : loading ? (
         <div>Loading...</div>
+      ) : (
+        <div>{error}</div>
       )}
     </div>
   );
---
+++ src/common/components/AssetTransaction/useTokenView.ts
@@ -0,0 +1,104 @@
+import React from 'react';
+import {useEffect, useState} from 'react';
+
+import useAssetMetadata from 'common/hooks/useAssetMetadata/useAssetMetadata';
+import numberFormatter from 'utils/numberFormatter';
+import {getTokenAmount} from 'utils/dataFormatting';
+import useToken from 'common/hooks/useToken/useToken';
+import {useCurrency} from 'common/currency/Currency.context';
+import {CurrencySymbol} from 'common/currency/Currency.types';
+import {TokenViewProps} from './AssetTransactionItem/TokenView';
+
+interface TokenInfo {
+  symbol: string;
+  icon: string;
+  tokenChange?: string;
+  currencyChange?: string;
+}
+
+interface TokenViewState {
+  tokenInfo: TokenInfo;
+  loading: boolean;
+  error: '';
+}
+
+// tokenChange =
+//{tokenAmount.startsWith('-') ? '' : '+'}
+//{numberFormatter({value: tokenAmount, size: 2})} {tokenInfo?.symbol}
+
+// currencyChange
+//tokenAmountInCurrency ? (tokenAmountInCurrency.toString().startsWith('-') ? '-' : '+') + currStr + numberFormatter({value: tokenAmountInCurrency, size: 1}).replace('-', '') : ''
+
+const getTokenAmountInCurrency = (amount: string, price: string, currencySymbol: string) => {
+  let value = (Number(amount) * Number(price)).toString();
+
+  const isNegative = value.startsWith('-');
+  if (isNegative) value = value.replace('-', '');
+
+  const operator = isNegative ? '-' : '+';
+
+  return `${operator}${currencySymbol}${numberFormatter({value: value, size: 1})}`;
+};
+
+const getTokenAmountString = (amount: string, tokenSymbol: string) => {
+  let tokenAmountStr = amount;
+
+  const isNegative = tokenAmountStr.startsWith('-');
+  if (isNegative) tokenAmountStr = tokenAmountStr.replace('-', '');
+
+  const operator = isNegative ? '-' : '+';
+
+  return `${operator}${numberFormatter({value: tokenAmountStr, size: 2})} ${tokenSymbol}`;
+};
+
+const useTokenView = (token: TokenViewProps) => {
+  const {currency} = useCurrency();
+
+  const [state, setState] = useState<TokenViewState>({
+    tokenInfo: undefined,
+    loading: true,
+    error: '',
+  });
+
+  const tokenMetadata = useAssetMetadata(token.id, 1, token.tokenAddress);
+  const tokenData = useToken(token?.tokenAddress);
+
+  useEffect(() => {
+    if (tokenMetadata && tokenData) {
+      const currStr = CurrencySymbol[currency] || '';
+
+      const tokenInfo: TokenInfo = {
+        symbol: tokenData.symbol,
+        icon: tokenMetadata.image.small,
+      };
+
+      // case when transaction involves token transfers
+      if (token.tokenValue) {
+        const tokenAmount = getTokenAmount(token?.tokenValue, tokenData.decimals);
+
+        const tokenPrice = tokenMetadata?.market_data.current_price.find(
+          (p) => p.currency === currency.toLocaleLowerCase(),
+        );
+
+        if (tokenAmount && tokenPrice) {
+          tokenInfo.currencyChange = getTokenAmountInCurrency(
+            tokenAmount,
+            tokenPrice.price,
+            currStr,
+          );
+          tokenInfo.tokenChange = getTokenAmountString(tokenAmount, tokenData.symbol);
+        }
+      }
+
+      setState({
+        loading: false,
+        tokenInfo: tokenInfo,
+        error: null,
+      });
+    }
+  }, [tokenMetadata, tokenData]);
+
+  return state;
+};
+
+export default useTokenView;
