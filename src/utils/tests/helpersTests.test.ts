f5092b5a8371f2e1144702fba51df6239c1f32c7
Volodymyr Barybin <vohanchib@gmail.com>
2022-06-03 15:28:27 +0300

    rm word test from test-cases

--- src/utils/tests/helpersTests.test.ts
+++ src/utils/tests/helpersTests.test.ts
@@ -1,7 +1,7 @@
 import {filteredDropdown, fromBnToNumber, getStringFromPath, rmCommasFromNum, shortenedAddress} from 'utils/helpers';
 import {detectAssetOrProtocolPage} from 'utils/helpers';
 
-test('Test converting from BN with defined decimal', () => {
+test('Converting from BN with defined decimal', () => {
   const result = fromBnToNumber('1000000000000000000000', 18);
   const resultAlt = fromBnToNumber('1000000000', 6);
   const result_alt = fromBnToNumber('100000000000', 8);
@@ -25,7 +25,7 @@ test('Detect Page by pathname', () => {
   expect(result_asset).toBe('asset');
 });
 
-describe('Testing filtered dropdown', () => {
+describe('Filtered dropdown', () => {
   const array = [
     {
       id: 1,
@@ -44,7 +44,7 @@ describe('Testing filtered dropdown', () => {
       ok: true,
     },
   ];
-  it('testing correct values', () => {
+  it('Correct values', () => {
     const result = filteredDropdown(array, 3);
     expect(result).toEqual([
       {
@@ -61,35 +61,35 @@ describe('Testing filtered dropdown', () => {
       },
     ])
   })
-  it('testing unpredicted values', () => {
+  it('Unpredicted values', () => {
     const result = filteredDropdown(array, 15);
 
     expect(result).toEqual(array);
   })
-  it('testing undefined values', () => {
+  it('Undefined values', () => {
     const result = filteredDropdown(undefined, 1);
 
     expect(result).toBe(undefined);
   })
 })
 
-describe('Testing removing commas from value', () => {
-  it('testing number value with comma', () => {
+describe('Removing commas from value', () => {
+  it('Number value with comma', () => {
     const result = rmCommasFromNum('10,000');
 
     expect(result).toBe('10000');
   });
-  it('testing value without comma', () => {
+  it('Value without comma', () => {
     const result = rmCommasFromNum('10000');
 
     expect(result).toBe('10000');
   });
-  it('testing value without more than one comma', () => {
+  it('Value without more than one comma', () => {
     const result = rmCommasFromNum('100,000,000');
 
     expect(result).toBe('100000000')
   });
-  it('testing unpredictable values', () => {
+  it('Unpredictable values', () => {
     const resultUndefinedCase = rmCommasFromNum(undefined);
     const resultNullCase = rmCommasFromNum(null);
     const resultEmptyString = rmCommasFromNum('');
@@ -100,13 +100,13 @@ describe('Testing removing commas from value', () => {
   })
 })
 
-describe('Testing ejecting fragment from pass string', () => {
-  it('testing correct values', () => {
+describe('Ejecting fragment from pass string', () => {
+  it('Correct values', () => {
     const result = getStringFromPath('/account/0xe9r8g49ds194tg9e8t4g9s1/network/1/assets/fwb', 3);
 
     expect(result).toBe('network');
   })
-  it('testing unpredictable values', () => {
+  it('Unpredictable values', () => {
     const resultUndefinedValue = getStringFromPath(undefined, 3);
     const resultNullValue = getStringFromPath(undefined, 3);
     const resultWithOutOfRangeIndex = getStringFromPath('/account/0xe9r8g49ds194tg9e8t4g9s1/network/1/assets/fwb', 15);
@@ -121,13 +121,13 @@ describe('Testing ejecting fragment from pass string', () => {
   })
 })
 
-describe('Testing shortened function', () => {
-  it('Testing correct data', () => {
+describe('Shortened function', () => {
+  it('Correct data', () => {
     const resultWithNonZeroSize = shortenedAddress('0xa79e63e78eec28741e711f89a672a4c40876ebf3', 4);
 
     expect(resultWithNonZeroSize).toBe('0xa7...ebf3')
   });
-  it('Testing correct data with limit size values', () => {
+  it('Correct data with limit size values', () => {
     const resultWithZeroSize = shortenedAddress('0xa79e63e78eec28741e711f89a672a4c40876ebf3', 0);
     const resultWithLimitedSize = shortenedAddress('0xa79e63e78eec28741e711f89a672a4c40876ebf3', 19);
     const resultWithLimitedSize_alt = shortenedAddress('0xa79e63e78eec28741e711f89a672a4c40876ebf3', 20);
@@ -136,7 +136,7 @@ describe('Testing shortened function', () => {
     expect(resultWithLimitedSize).toBe('0xa79e63e78eec28741...f89a672a4c40876ebf3');
     expect(resultWithLimitedSize_alt).toBe('0xa79e63e78eec28741e711f89a672a4c40876ebf3');
   });
-  it('Testing unpredictable inputs', () => {
+  it('Unpredictable inputs', () => {
     const resultWithUndefined = shortenedAddress(undefined, 1);
     const resultWithNull = shortenedAddress(null, 1);
     const resultWithUndefinedSize = shortenedAddress('0xa79e63e78eec28741e711f89a672a4c40876ebf3', undefined);
--- src/utils/tests/numberFormatter.test.ts
+++ src/utils/tests/numberFormatter.test.ts
@@ -1,7 +1,7 @@
 import numberFormatter from "utils/numberFormatter";
 
-describe('Testing number formatter', () => {
-  test('Testing number formatter on different predicted inputs', () => {
+describe('Number formatter', () => {
+  test('Number formatter on different predicted inputs', () => {
     const resultWithNumberInput = numberFormatter({value: 10000, size: 2});
     const resultWithStringInput = numberFormatter({value: '10000', size: 2});
     const resultWithZeroSize = numberFormatter({value: 10000, size: 0});
@@ -11,7 +11,7 @@ describe('Testing number formatter', () => {
     expect(resultWithZeroSize).toBe('10,000');
   })
 
-  test('Testing number formatter on different unpredicted inputs', () => {
+  test('Number formatter on different unpredicted inputs', () => {
     const resultWithUndefinedValue = numberFormatter({value: undefined, size: 2});
     const resultWithNullValue = numberFormatter({value: null, size: 2});
 
@@ -19,7 +19,7 @@ describe('Testing number formatter', () => {
     expect(resultWithNullValue).toBe('0');
   })
 
-  test('Testing number formatter on zero values', () => {
+  test('Number formatter on zero values', () => {
     const resultWithZeroValueAndNonZeroSize = numberFormatter({value: 0, size: 2});
     const resultWithZeroValueAndZeroSize = numberFormatter({value: 0, size: 0});
 
