f10e8cce3e8c32577575acbc168fc881c47086fb
Volodymyr Barybin <vohanchib@gmail.com>
2022-06-06 21:15:34 +0300

    fixed table styles

--- src/common/components/ProtocolPage/Vaults/VaultsTableItem/VaultsTableItem.tsx
+++ src/common/components/ProtocolPage/Vaults/VaultsTableItem/VaultsTableItem.tsx
@@ -6,6 +6,7 @@ import classNames from 'classnames';
 import {CurrencySymbol} from 'common/currency/Currency.types';
 import { AssetData } from '../../shared/types';
 import { getAssetIcons } from '../../shared/utils';
+import MenuArrow from 'common/components/MenuArrow/MenuArrow';
 
 interface Props {
   asset: AssetData;
@@ -46,6 +47,9 @@ function VaultsItem({asset, onClick}: Props) {
           >
             <div className={styles.title}>
               <div className={styles.valueTitle}>{toFixed(valueStr, 2)}</div>
+            </div>
+            <div className={styles.arrowIcon}>
+              <MenuArrow startPosition='right' />
             </div>
           </div>
         </div>
