e1d55f57af64aa21b99a0964bfef7fda2a3d37dc
Brioline <33597141+Brioline@users.noreply.github.com>
2022-06-07 19:08:35 +0300

    Claimable value (#153)
    * test patch]
    
    * claimable table value
    
    * icon for yearn protocol

--- src/utils/tests/numberFormatter.test.ts
+++ src/utils/tests/numberFormatter.test.ts
@@ -1,29 +1,29 @@
-import numberFormatter from "utils/numberFormatter";
+import numberFormatter from 'utils/numberFormatter';
 
 describe('Number formatter', () => {
   test('Number formatter on different predicted inputs', () => {
-    const resultWithNumberInput = numberFormatter({value: 10000, size: 2});
-    const resultWithStringInput = numberFormatter({value: '10000', size: 2});
-    const resultWithZeroSize = numberFormatter({value: 10000, size: 0});
-  
+    const resultWithNumberInput = numberFormatter(10000);
+    const resultWithStringInput = numberFormatter('10000');
+    const resultWithZeroSize = numberFormatter(10000);
+
     expect(resultWithNumberInput).toBe('10,000.00');
     expect(resultWithStringInput).toBe('10,000.00');
     expect(resultWithZeroSize).toBe('10,000');
-  })
+  });
 
   test('Number formatter on different unpredicted inputs', () => {
-    const resultWithUndefinedValue = numberFormatter({value: undefined, size: 2});
-    const resultWithNullValue = numberFormatter({value: null, size: 2});
+    const resultWithUndefinedValue = numberFormatter(undefined);
+    const resultWithNullValue = numberFormatter(null);
 
     expect(resultWithUndefinedValue).toBe('0');
     expect(resultWithNullValue).toBe('0');
-  })
+  });
 
   test('Number formatter on zero values', () => {
-    const resultWithZeroValueAndNonZeroSize = numberFormatter({value: 0, size: 2});
-    const resultWithZeroValueAndZeroSize = numberFormatter({value: 0, size: 0});
+    const resultWithZeroValueAndNonZeroSize = numberFormatter(0);
+    const resultWithZeroValueAndZeroSize = numberFormatter(0);
 
     expect(resultWithZeroValueAndNonZeroSize).toBe('0');
     expect(resultWithZeroValueAndZeroSize).toBe('0');
-  })
-})
+  });
+});
