c37b91312eea735f6d33f25263bc9698ebc99942
Volodymyr Barybin <vohanchib@gmail.com>
2022-06-07 15:29:02 +0300

    fixed number formatter func tests

--- src/utils/tests/numberFormatter.test.ts
+++ src/utils/tests/numberFormatter.test.ts
@@ -2,28 +2,72 @@ import numberFormatter from "utils/numberFormatter";
 
 describe('Number formatter', () => {
   test('Number formatter on different predicted inputs', () => {
-    const resultWithNumberInput = numberFormatter({value: 10000, size: 2});
-    const resultWithStringInput = numberFormatter({value: '10000', size: 2});
-    const resultWithZeroSize = numberFormatter({value: 10000, size: 0});
+    const resultWithNumberInput = numberFormatter(10000);
+    const resultWithStringInput = numberFormatter('10000');
+
+    const resultWithMinFractionOption = numberFormatter(10000, {minimumFractionDigits: 2});
+
+    const resultWithMaxFractionOption_1 = numberFormatter(10000, {maximumFractionDigits: 2});
+    const resultWithMaxFractionOption_2 = numberFormatter(10000.0, {maximumFractionDigits: 2});
+    const resultWithMaxFractionOption_3 = numberFormatter(10000.1, {maximumFractionDigits: 2});
+    const resultWithMaxFractionOption_4 = numberFormatter(10000.15, {maximumFractionDigits: 2});
+    const resultWithMaxFractionOption_5 = numberFormatter(10000.153, {maximumFractionDigits: 2});
+
+    const resultWithMaxAndMinFractionOption_1 = numberFormatter(
+      10000.153,
+      {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2
+      }
+    );
+    const resultWithMaxAndMinFractionOption_2 = numberFormatter(
+      10000.1,
+      {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2
+      }
+    );
+    const resultWithMaxAndMinFractionOption_3 = numberFormatter(
+      10000,
+      {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2
+      }
+    );
   
-    expect(resultWithNumberInput).toBe('10,000.00');
-    expect(resultWithStringInput).toBe('10,000.00');
-    expect(resultWithZeroSize).toBe('10,000');
+    expect(resultWithNumberInput).toBe('10,000');
+    expect(resultWithStringInput).toBe('10,000');
+
+    expect(resultWithMinFractionOption).toBe('10,000.00');
+
+    expect(resultWithMaxFractionOption_1).toBe('10,000');
+    expect(resultWithMaxFractionOption_2).toBe('10,000');
+    expect(resultWithMaxFractionOption_3).toBe('10,000.1');
+    expect(resultWithMaxFractionOption_4).toBe('10,000.15');
+    expect(resultWithMaxFractionOption_5).toBe('10,000.15');
+
+    expect(resultWithMaxAndMinFractionOption_1).toBe('10,000.15');
+    expect(resultWithMaxAndMinFractionOption_2).toBe('10,000.10');
+    expect(resultWithMaxAndMinFractionOption_3).toBe('10,000.00');
   })
 
   test('Number formatter on different unpredicted inputs', () => {
-    const resultWithUndefinedValue = numberFormatter({value: undefined, size: 2});
-    const resultWithNullValue = numberFormatter({value: null, size: 2});
+    const resultWithUndefinedValue = numberFormatter(undefined);
+    const resultWithNullValue = numberFormatter(null);
 
     expect(resultWithUndefinedValue).toBe('0');
     expect(resultWithNullValue).toBe('0');
   })
 
   test('Number formatter on zero values', () => {
-    const resultWithZeroValueAndNonZeroSize = numberFormatter({value: 0, size: 2});
-    const resultWithZeroValueAndZeroSize = numberFormatter({value: 0, size: 0});
+    const resultWithZeroValueAndNonZeroSize = numberFormatter(0);
+    const resultWithMinFractionOption = numberFormatter(0, {minimumFractionDigits: 2});
+    const resultWithMaxFractionOption = numberFormatter(0, {maximumFractionDigits: 2});
+    const resultWithZeroMaxFractionOption = numberFormatter(0.14567, {maximumFractionDigits: 0});
 
     expect(resultWithZeroValueAndNonZeroSize).toBe('0');
-    expect(resultWithZeroValueAndZeroSize).toBe('0');
+    expect(resultWithMinFractionOption).toBe('0');
+    expect(resultWithMaxFractionOption).toBe('0');
+    expect(resultWithZeroMaxFractionOption).toBe('0');
   })
 })
